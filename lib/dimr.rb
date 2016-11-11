d9d7d74a0aba35b5fffd7dc9cf1c1e340e276720
Steve England <stephen.england@gmail.com>
2016-11-11 17:30:18 +0000

    Making Dimr a module

--- lib/dimr.rb
+++ lib/dimr.rb
@@ -1,44 +1,49 @@
 require "dimr/version"
+require 'forwardable'
 require "dim"
 
 module Dimr
-  class Container < Dim::Container
-    def initialize(parent=nil, &block)
-      super(parent)
-      self.instance_eval(&block)
+  extend Forwardable
+
+  private
+
+  def run(runable_klass, dependencies, method = :run!)
+    factory(runable_klass, dependencies, method)
+  end
+  alias :command :run
+  alias :query :run
+
+  def factory(runable_klass, dependencies, method = nil)
+    Runner.new(runable_klass, dependencies, method)
+  end
+
+  def container
+    @container ||= Dim::Container.new
+  end
+
+  def_delegators :container, :register_env, :register, :method_missing
+
+  class Runner
+    attr_reader :runable_klass, :dependencies, :method
+
+    def initialize(runable_klass, dependencies, method = nil)
+      @runable_klass, @dependencies, @method =
+        runable_klass, dependencies, method
     end
 
-    def run(runable_klass, dependencies, method = :run!)
-      factory(runable_klass, dependencies, method)
-    end
-    alias :command :run
-    alias :query :run
+    def call(*args)
+      runable = runable_klass.new(*args)
 
-    def factory(runable_klass, dependencies, method = nil)
-      Runner.new(runable_klass, dependencies, method)
-    end
+      dependencies.each do |key, value|
+        runable.send("#{key}=", value)
+      end if dependencies
 
-    class Runner
-      attr_reader :runable_klass, :dependencies, :method
-
-      def initialize(runable_klass, dependencies, method = nil)
-        @runable_klass, @dependencies, @method =
-          runable_klass, dependencies, method
-      end
-
-      def call(*args)
-        runable = runable_klass.new(*args)
-
-        dependencies.each do |key, value|
-          runable.send("#{key}=", value)
-        end if dependencies
-
-        if method
-          runable.public_send(method)
-        else
-          runable
-        end
+      if method
+        runable.public_send(method)
+      else
+        runable
       end
     end
   end
+
 end
--- spec/dimr_spec.rb
+++ spec/dimr_spec.rb
@@ -13,41 +13,41 @@ describe Dimr do
     expect(Dimr::VERSION).not_to be nil
   end
 
-  describe Dimr::Container do
-    subject {
-      Dimr::Container.new do
-        register :my_command do
-          run(
-            Runable,
-            service: Service,
-            config: :test
-          )
-        end
+  subject {
+    Module.new do
+      extend Dimr
 
-        register :my_factory do
-          factory(
-            Runable,
-            service: Service,
-            config: :test
-          )
-        end
+      register :my_command do
+        run(
+          Runable,
+          service: Service,
+          config: :test
+        )
       end
-    }
 
-    describe '#run' do
-      it 'runs the command on the service' do
-        expect(subject.my_command.(some: 'args')).to eq :success
+      register :my_factory do
+        factory(
+          Runable,
+          service: Service,
+          config: :test
+        )
       end
     end
+  }
 
-    describe '#factory' do
-      it 'returns an instance of the runable' do
-        expect(subject.my_factory.(some: 'args')).to be_a Runable
-      end
+  describe '#run' do
+    it 'runs the command on the service' do
+      expect(subject.my_command.(some: 'args')).to eq :success
     end
   end
 
-  describe Dimr::Container::Runner do
+  describe '#factory' do
+    it 'returns an instance of the runable' do
+      expect(subject.my_factory.(some: 'args')).to be_a Runable
+    end
+  end
+
+  describe Dimr::Runner do
     subject {
       described_class.new(Runable, service: Service, config: :test)
     }
